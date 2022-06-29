20891693a8e0ef18215208c1be94e2a2f56655b2
Sachin <sachinshet135@gmail.com>
2022-06-29 23:17:57 +0530

    added a new line in the file

--- Practice.java
+++ Practice.java
@@ -2,5 +2,7 @@ public class Practice{
     public static void main(String args[])
     {
         System.out.println("Hello ,World!");
+        System.out.println("Cheers to have started learning java and DSA");
+
     }
 }
