c8d31e6451f60e44d937c1bf174763b850537025
Sachin <sachinshet135@gmail.com>
2022-08-15 03:07:12 +0530

    traversal of array using for each loop

--- Practice.java
+++ Practice.java
@@ -4,10 +4,9 @@ class Practice{
     public static void main(String args[])
     {
         int[] arr = {1,2,3,4,5};
-        for (int i=0; i<arr.length; i++) 
+        for (int i:arr) 
         { 
-            int var = arr[i];
-            System.out.println(arr[i]);
+            System.out.println(i);
         }
 }
 }
