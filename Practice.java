4efcf1cde0a214e6c3067cd8cb205d8bbfbd5a0a
Sachin <sachinshet135@gmail.com>
2022-08-15 01:55:51 +0530

    testing out arguments

--- Practice.java
+++ Practice.java
@@ -1,6 +1,6 @@
 //Simple script to understand the properties and behavior of an object
 
-class Car{
+class Practice{
     static int mileage  = 18;
     static int cost  = 1500000;
     static String name = "Nexon";
@@ -23,11 +23,15 @@ class Car{
 
     public static void main(String args[])
     {
-        Car c1 = new Car();
+        Practice c1 = new Practice();
         c1.start();
         c1.accelerate();
         int number = c1.stop();
 
         System.out.println(number);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
     }
 }
