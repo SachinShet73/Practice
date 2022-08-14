0d1a9521f5873b55c8a1f7df49ff65dac4fd3b29
Sachin <sachinshet135@gmail.com>
2022-08-15 02:15:07 +0530

    simple commit

--- Practice.java
+++ Practice.java
@@ -1,37 +1,11 @@
 //Simple script to understand the properties and behavior of an object
 
 class Practice{
-    static int mileage  = 18;
-    static int cost  = 1500000;
-    static String name = "Nexon";
-
-    void start()
-    {
-        System.out.println("The car has started");
-    }
-
-    void accelerate()
-    {
-        System.out.println("The car has accelerated");
-    }                                                                                                                        
-
-    int stop()
-    {
-        System.out.println("The car has stopped");
-        return 2;
-    }
-
     public static void main(String args[])
     {
-        Practice c1 = new Practice();
-        c1.start();
-        c1.accelerate();
-        int number = c1.stop();
-
-        System.out.println(number);
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
-        System.out.println(args[3]);
+        int a , b;
+        a = 5;
+        b = ++a + a++ ;
+        System.out.println(b);
     }
 }
