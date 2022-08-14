b5587fe7de2407de6931abe112d6702eb3f6f28e
Sachin <sachinshet135@gmail.com>
2022-08-15 01:08:50 +0530

    Simple script too understand the properties and behavior of an object

--- Practice.java
+++ Practice.java
@@ -1,8 +1,34 @@
-public class Practice{
+//Simple script to understand the properties and behavior of an object
+
+class Car{
+    static int mileage  = 18;
+    static int cost  = 1500000;
+    static String name = "Nexon";
+
+    static void start()
+    {
+        System.out.println("The car has started");
+    }
+
+    static void accelerate()
+    {
+        System.out.println("The car has accelerated");
+    }
+
+    static void stop()
+    {
+        System.out.println("The car has stopped");
+    }
+
     public static void main(String args[])
     {
-        System.out.println("Hello ,World!");
-        System.out.println("Cheers to have started learning java and DSA");
+        System.out.println("The mileage of the car is %d" +mileage);
+        System.out.println("The mileage of the car is  " + cost);
+        System.out.println("The mileage of the car is  " + name);
+
+        start();
+        accelerate();
+        stop();
 
     }
 }
