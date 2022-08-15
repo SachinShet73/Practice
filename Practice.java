dd017eba7873b80cc0bbde6dd5f327e470b17e6c
Sachin <sachinshet135@gmail.com>
2022-08-15 17:15:51 +0530

    1st program of pattern programming

--- Practice.java
+++ Practice.java
@@ -3,10 +3,11 @@
 class Practice{
     public static void main(String args[])
     {
-        int[] arr = {1,2,3,4,5};
-        for (int i:arr) 
-        { 
-            System.out.println(i);
+        int[][] arr ;
+
+        for(int i = 0;i<1;i++)
+        {
+            System.out.print("#");
         }
 }
 }
