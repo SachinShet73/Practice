62f2e7b9557819d0e21c97d2f9ef385132ca99cf
Sachin <sachinshet135@gmail.com>
2022-08-15 17:26:29 +0530

    Program to print a right angled triangle

--- Practice.java
+++ Practice.java
@@ -5,9 +5,9 @@ class Practice{
     {
         int[][] arr ;
 
-        for(int i = 0;i<5;i++)
+        for(int i = 1;i<6;i++)
         {
-            for (int j = 0;j<5;j++)
+            for (int j = 1;j<=i;j++)
             {
                 System.out.print("#");
             } 
