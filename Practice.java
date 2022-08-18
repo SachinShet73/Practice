f0a0c9854610bf69efde65d6fa0894f6104c4062
Sachin <sachinshet135@gmail.com>
2022-08-18 16:11:56 +0530

    pattern programming problem 12

--- Practice.java
+++ Practice.java
@@ -4,15 +4,16 @@ class Practice{
     public static void main(String args[])
     {
         int[][] arr ;
-        for(int j = 1;j<=5;j++)
+        for(int row = 1;row<=5;row++)
         {
-        for(int i = j;i<=5;i++ )
+            for(int col = 1;col<=row;col++ )
         {
             System.out.print(" ");
         }
-        for(int k = 1;k<=2*j-1;k++)
+        
+            for(int col2 = 1 ; col2<= 11-2*row; col2++)
         {
-            System.out.print("#");
+            System.out.print("*");
         }
 
         System.out.println();
