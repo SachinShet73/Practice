eb013fd5e2a47264b783400f7ff35b9714c01d62
Sachin <sachinshet135@gmail.com>
2022-08-18 16:36:30 +0530

    Example 13

--- Practice.java
+++ Practice.java
@@ -3,17 +3,32 @@
 class Practice{
     public static void main(String args[])
     {
-        int[][] arr ;
+       
         for(int row = 1;row<=5;row++)
         {
-            for(int col = 1;col<=row;col++ )
+            for(int col = 1;col<=1;col++ )
         {
             System.out.print(" ");
         }
         
-            for(int col2 = 1 ; col2<= 11-2*row; col2++)
+            for(int col2 = 1 ; col2<= 5; col2++)
         {
-            System.out.print("*");
+            if(row == 2||row == 3||row == 4)
+            {
+                if( col2==2|| col2==3|| col2==4)
+                {   
+                    System.out.print(" ");
+                }
+
+                if(col2 ==1||col2==5)
+                { 
+                    System.out.print("*");
+                }
+            }
+                else
+                {
+                    System.out.print("*");
+                }
         }
 
         System.out.println();
