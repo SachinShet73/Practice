7e9f697677ca80cb00074e20592f88d3e66d131b
Sachin <sachinshet135@gmail.com>
2022-08-18 15:30:07 +0530

    pattern programming example 11

--- Practice.java
+++ Practice.java
@@ -6,11 +6,11 @@ class Practice{
         int[][] arr ;
         for(int j = 1;j<=5;j++)
         {
-        for(int i = j;i<=5;i++)
+        for(int i = j;i<=5;i++ )
         {
             System.out.print(" ");
         }
-        for(int k = 1;k<=5;k++)
+        for(int k = 1;k<=2*j-1;k++)
         {
             System.out.print("#");
         }
