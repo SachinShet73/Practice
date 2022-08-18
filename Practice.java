4b6f3bd32cf5a9b2ee37dc21c11adb910782aab4
Sachin <sachinshet135@gmail.com>
2022-08-18 16:37:18 +0530

    Example for pattern programming 14

--- Practice.java
+++ Practice.java
@@ -6,7 +6,7 @@ class Practice{
        
         for(int row = 1;row<=5;row++)
         {
-            for(int col = 1;col<=1;col++ )
+            for(int col = 1;col<=5;col++ )
         {
             System.out.print(" ");
         }
