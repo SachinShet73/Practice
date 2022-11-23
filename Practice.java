43ba2020426e38f952a29105829056a595f0694f
Sachin <sachinshet135@gmail.com>
2022-11-23 19:58:38 +0530

    invertedPyramidHollow pattern

--- Practice.java
+++ Practice.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
 
 import javax.lang.model.util.ElementScanner14;
-
 class Practice{
     /**
      * @param args
      */
+    public static void invertedPyramidHollow(int totlength)
+     {
+        for(int row = 1 ; row<=totlength;row++)
+        {
+            for(int col = 1 ; col<=totlength-row+1; col++)
+            {
+                    if(row == 1|| col == 1 || col==totlength-row+1)
+                    {
+                    System.out.print("*");
+                    }
+                    else
+                    {
+                    System.out.print(" ");
+                    }
+            }
+            System.out.println();    
+        }
+            
+        }
+     
     public static void main(String args[])
     {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a number to find the factorial of");
+    System.out.println("Enter the side of the half pyramid");
     int n = sc.nextInt();
-    int factorial = n;
-    int i = n;
-    while( i>0)
-    {   
-        if(n==1)
-        {
-            break;
-        }
-        factorial = factorial*(n-1);
-        n--;
-        i--;
+    invertedPyramidHollow(n);
     }
- 
-    System.out.println("The factorial of the number is " + factorial);
-
-    
-  
 }
-}
-
