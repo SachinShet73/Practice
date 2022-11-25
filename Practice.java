0a9e65922dde0bc60147f3b4a4afb0da3fccf2b8
Sachin <sachinshet135@gmail.com>
2022-11-25 13:16:47 +0530

    diamond pattern

--- Practice.java
+++ Practice.java
@@ -58,8 +58,6 @@ class Practice{
 
         }
 
-
-
     public static void hollow_rhombus(int n)
     {
         for(int row = 1;row<=n;row++)
@@ -87,9 +85,6 @@ class Practice{
 
     }
 
-
- 
-
     public static void solid_rhombus(int n)
     {
         for(int row = 1;row<=n;row++)
@@ -108,13 +103,73 @@ class Practice{
             System.out.println();
         }
 
+
     }   
+
+    public static void zero_one_triangle(int totlength)
+    {  
+      
+        byte p = 1;
+       for(int row = 1 ; row<=totlength;row++)
+       {   
+           for(int col = 1 ; col<=row; col++)
+           {
+                if((row+col)%2==0)
+                {
+               System.out.print(1 + " ");
+                }
+                else
+                {
+                    System.out.print(0 +" ");
+                }
+
+           }
+           System.out.println();    
+       }
+    }
+
+    public static void diamond(int n)
+    {
+        for (int row = 1; row<=n; row++)
+        {
+            for (int space = 1;space <= n+1-row;space++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int star = 1;star <= 2*row-1 ;star++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        
+        for (int row = 1; row<=n-1; row++)
+        {
+            for (int space = 1;space <= row+1;space++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int star = 1;star <= (n*2)-2*row-1 ;star++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }   
+    }
 
     public static void main(String args[])
     {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the input for hollow rhombus pattern");
     int n = sc.nextInt();
-    hollow_rhombus(n);
+
+    diamond(n);
+
     }
 }
+
+
